cbd2474d22f8d6b496504535209a4a45bbf67c73
Alexander Verbruggen <alex@nabu.be>
2020-03-03 18:57:14 +0100

    updated logic to fetch fields from table

--- src/main/java/be/nabu/libs/services/jdbc/JDBCUtils.java
+++ src/main/java/be/nabu/libs/services/jdbc/JDBCUtils.java
@@ -15,28 +15,44 @@ import be.nabu.libs.types.api.Type;
 import be.nabu.libs.types.properties.CollectionNameProperty;
 import be.nabu.libs.types.properties.ForeignKeyProperty;
 import be.nabu.libs.types.properties.PrimaryKeyProperty;
+import be.nabu.libs.types.properties.RestrictProperty;
 
 public class JDBCUtils {
-	private static void getFieldsInTable(ComplexType type, Map<String, Element<?>> children) {
-		// we start with the local children
+	private static void getFieldsInTable(ComplexType type, Map<String, Element<?>> children, boolean isRoot, List<String> restrictions) {
+		String typeCollectionName = getTypeName(type, isRoot);
+		
+		// we start with the local children that were not restricted from above
 		for (Element<?> child : type) {
-			if (!children.containsKey(child.getName())) {
+			if (!children.containsKey(child.getName()) && !restrictions.contains(child.getName())) {
 				children.put(child.getName(), child);
 			}
 		}
+		
+		String restriction = ValueUtils.getValue(RestrictProperty.getInstance(), type.getProperties());
+		if (restriction != null) {
+			restrictions.addAll(Arrays.asList(restriction.split("[\\s]*,[\\s]*")));
+		}
+		
 		Type superType = type.getSuperType();
 		// if we extend a complex type, check if it has its own collection name
 		if (superType instanceof ComplexType) {
 			String collectionName = ValueUtils.getValue(CollectionNameProperty.getInstance(), superType.getProperties());
-			// if it doesn't, it is absorbed into the current table
-			if (collectionName == null) {
-				getFieldsInTable((ComplexType) superType, children);
+			// if it doesn't or it is the same table, it is absorbed into the current table
+			if (collectionName == null || collectionName.equalsIgnoreCase(typeCollectionName)) {
+				getFieldsInTable((ComplexType) superType, children, false, restrictions);
 			}
 		}
 	}
+	private static String getTypeName(ComplexType type, boolean force) {
+		String typeCollectionName = ValueUtils.getValue(CollectionNameProperty.getInstance(), type.getProperties());
+		if (force && typeCollectionName == null) {
+			typeCollectionName = type.getName().replaceAll("([A-Z]+)", "_$1").replaceFirst("^_", "");
+		}
+		return typeCollectionName;
+	}
 	public static List<Element<?>> getFieldsInTable(ComplexType type) {
 		Map<String, Element<?>> children = new LinkedHashMap<String, Element<?>>();
-		getFieldsInTable(type, children);
+		getFieldsInTable(type, children, true, new ArrayList<String>());
 		return new ArrayList<Element<?>>(children.values());
 	}
 	
